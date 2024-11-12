e8fd767e1808ae6e777565c838a18795e214f0bb
habiba <habiba.hisham2811@gmail.com>
2024-11-12 17:10:07 +0100

    add motion

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,39 @@
 import { PROJECTS } from '../constants';
-
+import { motion } from 'framer-motion';
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">Projects</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h2>
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-12 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/3">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full lg:w-1/3"
+            >
               <img
                 src={project.image}
-                width={250} // Increase width
-                height={250} // Increase height
+                width={250}
+                height={250}
                 alt={project.title}
-                className="mb-6 rounded-lg shadow-lg" // Adjust roundedness and add shadow
+                className="mb-6 rounded-lg shadow-lg"
               />
-            </div>
-            <div className="w-full max-w-xl lg:w-2/3">
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl lg:w-2/3"
+            >
               <h6 className="mb-2 text-xl font-semibold">{project.title}</h6>
               <p className="mb-4 text-neutral-400">{project.description}</p>
               <div className="flex flex-wrap gap-2">
@@ -29,7 +46,7 @@ const Projects = () => {
                   </span>
                 ))}
               </div>
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,6 +3,7 @@ import { TbBrandNextjs } from 'react-icons/tb';
 import { DiRedis } from 'react-icons/di';
 import { BiLogoPostgresql } from 'react-icons/bi';
 import { FaNodeJs, FaDocker, FaAws } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 import {
   SiTypescript,
@@ -11,49 +12,160 @@ import {
   SiTerraform,
   SiMongodb,
 } from 'react-icons/si';
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: 'linear',
+      repeat: Infinity,
+      repeatType: 'reverse',
+    },
+  },
+});
+
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h2 className="my-20 text-center text-4xl">Technologies</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4 ">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4 "
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiReactjsLine className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <TbBrandNextjs className="text-7xl " />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <SiMongodb className="text-7xl text-green-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <DiRedis className="text-7xl text-red-700" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <FaNodeJs className="text-7xl text-green-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <SiTypescript className="text-7xl text-blue-600" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <BiLogoPostgresql className="text-7xl text-sky-700" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="text-7xl text-green-500"
+          />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiRedis
+            variants={iconVariants(5)}
+            initial="initial"
+            animate="animate"
+            className="text-7xl text-red-700"
+          />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs
+            variants={iconVariants(6)}
+            initial="initial"
+            animate="animate"
+            className="text-7xl text-green-500"
+          />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiTypescript
+            variants={iconVariants(7)}
+            initial="initial"
+            animate="animate"
+            className="text-7xl text-blue-600"
+          />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(7.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <BiLogoPostgresql
+            variants={iconVariants(8)}
+            initial="initial"
+            animate="animate"
+            className="text-7xl text-sky-700"
+          />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(8.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiGit className="text-7xl text-orange-600" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(9)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaDocker className="text-7xl text-blue-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(9.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiPowerbi className="text-7xl text-yellow-500" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(10)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaAws className="text-7xl text-orange-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(10.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiTerraform className="text-7xl text-purple-700" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
