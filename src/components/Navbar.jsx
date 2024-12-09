75ff63bfcef4d9cf8465187f2af0ad984a96fe45
habiba <habiba.hisham2811@gmail.com>
2024-12-09 15:09:42 +0100

    enhancing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,29 @@
-import logo from '../assets/logo.png';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-20" src={logo} alt="logo" />
-      </div>
+      <div className="flex flex-shrink-0 items-center"></div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
+        <motion.a
+          href="https://www.linkedin.com/in/habiba-soliman-00a59923a/"
+          target="_blank"
+          rel="noopener noreferrer"
+          whileHover={{ scale: 1.2 }}
+          transition={{ type: 'spring', stiffness: 300 }}
+        >
+          <FaLinkedin />
+        </motion.a>
+        <motion.a
+          href="https://github.com/habiba2811"
+          target="_blank"
+          rel="noopener noreferrer"
+          whileHover={{ scale: 1.2 }}
+          transition={{ type: 'spring', stiffness: 300 }}
+        >
+          <FaGithub />
+        </motion.a>
       </div>
     </nav>
   );
